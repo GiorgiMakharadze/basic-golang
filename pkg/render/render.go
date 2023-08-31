15e283ca999336d9d0308677121ec2904534836d
GiorgiMakharadze <makharadzegiorgi00@gmail.com>
2023-08-31 14:53:53 +0400

    template cahche pt2

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -1,8 +1,10 @@
 package render
 
 import (
+	"bytes"
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
 	"path/filepath"
 )
@@ -10,21 +12,27 @@ import (
 var functions = template.FuncMap {}
 
 func RenderTemplate(w http.ResponseWriter, html string) {
-
-	_, err := RenderTemplateTest(w)
+	tc, err := CreateTemplateCache()
 	if err != nil {
-		fmt.Println("Error getting template cache:", err )
+	log.Fatal(err)
 	}
 
-	parsedTemplate, _ := template.ParseFiles("./templates/" + html)
-	err = parsedTemplate.Execute(w, nil)
+	t, ok := tc[html]
+	if !ok {
+		log.Fatal(err)
+	}
+
+	buf := new(bytes.Buffer)
+	
+	_ = t.Execute(buf, nil)
+
+	_, err = buf.WriteTo(w)
 	if err != nil {
-		fmt.Println("error parsing template:", err)
-		return
+		fmt.Println("Error writing template to browser", err)
 	}
 }
 
-func RenderTemplateTest(w http.ResponseWriter) (map[string]*template.Template, error){
+func CreateTemplateCache() (map[string]*template.Template, error){
 	myCache := map[string]*template.Template{}
 
 	pages, err := filepath.Glob("./templates/*.page.html")
@@ -34,7 +42,6 @@ func RenderTemplateTest(w http.ResponseWriter) (map[string]*template.Template, e
 
 	for _, page := range pages {
 		name := filepath.Base(page)
-		fmt.Println("Page is currently", page)
 		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
 		if err != nil {
 			return myCache, err
