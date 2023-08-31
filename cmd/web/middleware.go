3e86f7f8dc1f4856314ec7ccde91b0c60f8cedef
GiorgiMakharadze <makharadzegiorgi00@gmail.com>
2023-08-31 16:57:20 +0400

    routing & middleware

---
+++ cmd/web/middleware.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/justinas/nosurf"
+)
+
+func NoSurf(next http.Handler) http.Handler {
+	csrfHandler := nosurf.New(next)
+
+	csrfHandler.SetBaseCookie(http.Cookie{
+		HttpOnly: true,
+		Path: "/",
+		// In production change Secure to true
+		Secure: false,
+		SameSite: http.SameSiteLaxMode,
+	})
+	return csrfHandler
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/GiorgiMakharadze/basic-golang
 
 go 1.18
+
+require github.com/go-chi/chi v1.5.4
+
+require github.com/justinas/nosurf v1.1.1 // indirect
