c01c933312a720a226c4363b60c6939bbf787303
GiorgiMakharadze <makharadzegiorgi00@gmail.com>
2023-08-31 17:25:02 +0400

    Sessions

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,17 +4,31 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/GiorgiMakharadze/basic-golang/pkg/config"
 	"github.com/GiorgiMakharadze/basic-golang/pkg/handlers"
 	"github.com/GiorgiMakharadze/basic-golang/pkg/render"
+	"github.com/alexedwards/scs/v2"
 )
 
 const portNumber = ":8080"
+var app config.AppConfig
+var session *scs.SessionManager
 
 
 func main() {
-	var app config.AppConfig
+
+	// In production set InProduction to true
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
 
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
